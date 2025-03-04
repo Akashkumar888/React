780e1be34021d25c111a1adcbf9b783b225083fd
Akashkumar888 <akashgkr12@gmail.com>
2025-03-05 02:20:11 +0530

    update

--- 18-Fragment/src/component/FoodItems.jsx
+++ 18-Fragment/src/component/FoodItems.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import Item from "./Item";
 
 const FoodItems = (props) => {
   let { items } = props; // Corrected from foodItems to items
+  
+  let [activeItems,setActiveItems]=useState([]);
 
   return (
     <ul className="list-group">
       {items.map((item) => (
-        <Item key={item} foodItem={item}
+        <Item 
+        key={item} 
+        foodItem={item}
+        bought={activeItems.includes(item)}
         handleBuyButtonClicked={()=>console.log(`${item} being bought`)}
         
         
--- 18-Fragment/src/component/Item.jsx
+++ 18-Fragment/src/component/Item.jsx
@@ -2,13 +2,13 @@ import styles from "./Item.module.css"; // Importing CSS module
 
 
 
-const Item = ({foodItem,handleBuyButtonClicked}) => {
+const Item = ({foodItem,bought,handleBuyButtonClicked}) => {
   // const handleBuyButtonClicked=(event)=>{
   //   console.log(event);
   //   console.log(`${foodItem} being bought`);
   // };
   return (
-    <li className={` ${styles['kg-item']} list-group-item `}> {/* Apply the correct class */}
+    <li className={` ${styles['kg-item']} list-group-item ${bought && 'active'}`}> {/* Apply the correct class */}
       <span className={styles['kg-span']}> {foodItem} </span>
       <button className={`${styles.button} btn btn-info`}
       onClick={handleBuyButtonClicked}
